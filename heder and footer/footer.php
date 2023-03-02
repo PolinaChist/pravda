e8d182c688f3079a18f337697bf74959e14fc8ba
PolinaChist <91689955+PolinaChist@users.noreply.github.com>
2023-03-02 19:38:59 +0500

    Add files via upload

---
+++ heder and footer/footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+    <div class="footer__container _container">
+        <div class="footer__row">
+            <div class="footer_block">
+                <div class="footer__nav">
+                    <a href="" class="footer__link">Главная</a>
+                    <a href="" class="footer__link">О нас</a>
+                    <a href="" class="footer__link">Программы</a>
+                    <a href="" class="footer__link">Контакты</a>
+                    <a href="" class="footer__link">Партнеры</a>
+                </div>
+                <div class="footer__rights">
+                    © 2022 Все права защищены
+                </div>
+            </div>
+            <div class="footer_logo">
+                <img src="img/logo/VaI3E0w0do4 11.png" class="log" alt="">
+            </div>
+            <div class="footer__icons">
+
+                <a class = "footer-icon-link" href="https://vk.com/pravdavmeste">
+                    <img src="img/icons/viber.svg" alt="">
+                </a>
+                <a class = "footer-icon-link" href="https://vk.com/pravdavmeste"><img src="img/icons/vk.png" alt=""></a>
+                <a class = "footer-icon-link" href="https://vk.com/pravdavmeste"><img src="img/icons/telega.png" alt=""></a>
+
+            </div>
+        </div>
+    </div>
+        
+       
+        
+</footer>
---
+++ heder and footer/header.php
@@ -0,0 +1,55 @@
+<header class="header">
+    <div class="header__wrapper">
+        <div class="header__container _container">
+            <div class="header__body">
+                <a href="index.php" class="header__logo">
+                    <img src="img/logo/VaI3E0w0do4 11.png" class="log" alt="">
+                </a>
+                <div class="header_burger">
+                    <span></span>
+                </div>
+                <nav class="header__menu">
+                    <ul class="header__list">
+                        <li>
+                            <a href="index.php" class="header__link">Главная</a>
+                        </li>
+                        <li>
+                            <a href="onas.php" class="header__link"> О нас</a>
+                        </li>
+                        <li>
+                            <a href="group.php" class="header__link"> Команда</a>
+                        </li>
+                        <li>
+                            <a href="programm.php" class="header__link">Программы</a>
+                        </li>
+                        <li>
+                            <a href="contact.php" class="header__link">Контакты</a>
+                        </li>
+                        <li>
+                            <a href="partners.php" class="header__link">Партнеры</a>
+                        </li>
+                    </ul>
+                    <div class="header__icons">
+                        <form class="header__sub-search" action="#" method = "post">
+                            <input type="text" class="header__input">
+                            <img src="img/icons/search.png" alt="" class="header__searchhhh">
+                        </form>
+                        <div class="header__iconss">
+                            <a href="tel:79091075814"><img src="img/icons/viber.svg
+" alt=""></a>
+                            <a href="https://vk.com/pravdavmeste"><img src="img/icons/vk.png" alt=""></a>
+                            <a href="https://vk.com/pravdavmeste"><img src="img/icons/telega.png" alt=""></a>
+                        </div>
+                        <a href="login.php" class="entrance">
+                            Вход
+                        </a>
+                    </div>
+                </nav>
+            </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script src="js/index.js"></script>
+    <script src="jquery-3.6.1.min.js"></script>
+
+</header>
